788b69a07b74a2806dcdb584f6f376327b507f3e
Artur Coelho <artur.rocha.coelho@gmail.com>
2022-09-23 00:14:58 -0300

    added home route

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import './App.css';
 import { Route, Routes, Navigate } from 'react-router-dom';
 import Layout from './components/Layout/Layout';
+import Home from './components/Home/Home';
 
 function App() {
   return (
     <Routes>
-      <Route path='/' element={<Layout />}></Route>
+      <Route path='/' element={<Layout />}>
+        <Route path='/home' element={<Home />}></Route>
+      </Route>
     </Routes>
   );
 }
